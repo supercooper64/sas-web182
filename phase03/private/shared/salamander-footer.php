95815259525c67e0ba15f40130f03d8d6739e88c
dj <48740685+supercooper64@users.noreply.github.com>
2025-03-25 11:32:17 -0400

    Delete phase03 directory

--- phase03/private/shared/salamander-footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
- &copy; <?php echo date('Y'); ?> Southern Appalachian Salamanders
- 
-</footer>
-
-</body>
-</html>
-  
-<?php db_disconnect($db); ?>
--- phase03/public/salamanders/edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('../../private/initialize.php');
-include(SHARED_PATH . '/salamander-header.php'); 
-
-echo "<h1>Stub for Edit Salamander</h1>";
-
-include(SHARED_PATH . '/salamander-footer.php'); ?>
